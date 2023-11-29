90d7896fac8dad2574f5a4e7359b3532bca2f4bb
Sukhman50 <140614380+Sukhman50@users.noreply.github.com>
2023-11-29 18:08:10 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,7 +1,8 @@
 
 	//Get "player1_name" using localStorage.getItem() function and assign it to player1_name
+	localStorage.setItem("player1_name", player1_name);
 	//Get "player2_name" using localStorage.getItem() function and assign it to player2_name
-
+    localStorage.setItem("player2_name", player2_name);
 	player1_score = 0;
 	player2_score = 0;
 
